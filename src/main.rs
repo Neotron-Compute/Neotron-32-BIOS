1491d005b82c67e7a76aa7cafa21f643399ec3c9
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2019-11-24 21:56:02 +0000

    Set license to GPL3.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,23 @@
 //! computer based upon the Texas Instruments Tiva-C Launchpad, which is
 //! powered by the TM4C123 SoC and its 80 MHz Cortex-M4F processor core. It
 //! has 32 KiB of SRAM (hence the name) and 256 KiB of Flash.
+//!
+//! ## License
+//!
+//!     Copyright (C) 2019 Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
+//!
+//!     This program is free software: you can redistribute it and/or modify
+//!     it under the terms of the GNU General Public License as published by
+//!     the Free Software Foundation, either version 3 of the License, or
+//!     (at your option) any later version.
+//!
+//!     This program is distributed in the hope that it will be useful,
+//!     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//!     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//!     GNU General Public License for more details.
+//!
+//!     You should have received a copy of the GNU General Public License
+//!     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #![no_main]
 #![no_std]
 

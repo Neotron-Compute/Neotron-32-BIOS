b00e708e38e6f3748a70fe736cc65dc244fb293a
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2020-07-19 11:34:28 +0100

    Rebuild if linker script changes.

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("cargo:rerun-if-changed=src/delay_spi.s");
+    println!("cargo:rerun-if-changed=neotron-32-bios.ld");
     cc::Build::new()
         .file("src/delay_spi.s")
         .compile("delay_spi");

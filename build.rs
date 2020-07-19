e8c0a62fa8674cf586055b7a65e49a8152ac768f
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2020-07-19 11:38:20 +0100

    Add intro text.

--- build.rs
+++ build.rs
@@ -1,3 +1,8 @@
+/// This is the build-script for the Neotron 32 BIOS
+///
+/// It basically tells Cargo which files are important and should trigger a
+/// rebuild. It also compiles any C or ASM components, using the `cc` crate.
+
 fn main() {
     println!("cargo:rerun-if-changed=src/delay_spi.s");
     println!("cargo:rerun-if-changed=neotron-32-bios.ld");
